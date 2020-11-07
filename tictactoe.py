3c73c1926cb7d3a10717745e5c8c17a8f87de597
KamalKirosingh <kamal.kirosingh16@gmail.com>
2020-11-07 01:56:16 +0000

    first function

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,34 @@
-#test
+"""
+@author: kamal
+"""
+from IPython.display import clear_output
+
+def display_board(board):
+    clear_output()
+    
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    
+    
+def player_input():
+    marker = 'WRONG'
+    
+    while marker not in ['X', 'O']:
+        marker = input("Would you like to be X or O? ")
+        
+        if marker not in ['X', 'O']:
+            print("I did not recognise that, please try again")
+
+    if marker == 'X':
+        return ('X', 'O')
+    else:
+        return ('O', 'X')
